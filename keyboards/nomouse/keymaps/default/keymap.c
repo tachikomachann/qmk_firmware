fb1e28c901a2ad34f542279638a421eb2587cda1
sheldon.cai <sheldon.cai@ringcentral.com>
2024-11-20 14:30:19 +0800

    base layout

--- keyboards/nomouse/keymaps/default/keymap.c
+++ keyboards/nomouse/keymaps/default/keymap.c
@@ -11,31 +11,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     Replace mouse key MS_BTN1 ~ MS_BTN3 to KC_PSLS, KC_PAST and KC_PMNS, 
     then use software(ahk) remap these keys to left/middle/right button
     **/
-        KC_TAB,  KC_Q,    KC_W,    KC_E,        KC_R,    KC_T,    KC_PSLS,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,        KC_F,    KC_G,                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
-        KC_LSFT, KC_Z,    KC_X,    KC_C,        KC_V,    KC_B,    KC_PMNS,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCAP, MO(1),   KC_LALT, LT(2,KC_SPC),         KC_PAST,             KC_ENT,           MO(3),   KC_RGUI, KC_RCTL, KC_MENU
+        KC_TAB,		   KC_Q,    KC_W,    KC_E,        KC_R,    KC_T,    KC_PSLS,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LCTL,	   KC_A,    KC_S,    KC_D,        KC_F,    KC_G,                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
+        KC_LSFT,	   KC_Z,    KC_X,    KC_C,        KC_V,    KC_B,    KC_PMNS,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        LT(4,KC_LCAP), MO(1),   KC_LALT, LT(2,KC_SPC),         KC_PAST,             KC_ENT,           MO(3),   KC_RGUI, KC_RCTL, KC_MENU
     ),
 
     [1] = LAYOUT( 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,             KC_TRNS,		    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,             KC_TRNS,		  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [2] = LAYOUT( 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,             KC_TRNS,		    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,             KC_TRNS,		  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [3] = LAYOUT( 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,             KC_TRNS,		    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,             KC_TRNS,		  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+
+    [4] = LAYOUT( 
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,	   KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,             KC_TRNS,		  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
 };
@@ -43,5 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD)  },
+    [1] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD)  },
+    [2] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD)  },
+    [3] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD)  },
 };
 #endif
