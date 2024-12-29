737b4a591edad4a88b77d9522e5d3559e36e81e8
tachikoma <caixiexin@outlook.com>
2024-12-29 10:40:38 +0800

    add INS keycode

--- keyboards/nomouse/keymaps/default/keymap.c
+++ keyboards/nomouse/keymaps/default/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
     [3] = LAYOUT( 
-        KC_GRV,		        KC_F1,  		KC_F2,		    KC_F3,		    KC_F4,		    KC_F5,						    KC_F6,		    KC_F7,		    KC_F8,		    KC_F9,		    KC_F10,		    KC_TRNS,	KC_TRNS,			
+        KC_GRV,		        KC_F1,  		KC_F2,		    KC_F3,		    KC_F4,		    KC_F5,						    KC_F6,		    KC_F7,		    KC_F8,		    KC_F9,		    KC_F10,		    KC_TRNS,	KC_INS,			
         KC_TRNS,			KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS,						KC_TRNS,		KC_TRNS,		KC_UP,		    KC_TRNS,		KC_TRNS,	    KC_TRNS,    KC_TRNS,
         KC_TRNS,			KC_F11,		    KC_F12,		    KC_TRNS,		KC_TRNS,		KC_TRNS,						KC_TRNS,		KC_LEFT,		KC_DOWN,		KC_RIGHT,		KC_TRNS,	    KC_TRNS,	KC_TRNS,
         KC_TRNS,			KC_VOLD,	    KC_VOLU,	    KC_MPRV,	    KC_MNXT,		KC_MPLY,						KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS,			
