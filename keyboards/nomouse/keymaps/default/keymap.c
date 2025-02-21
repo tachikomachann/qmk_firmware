18ca0648b7f1d3abced50cfd3cbe681e40257c46
sheldon.cai <sheldon.cai@ringcentral.com>
2025-02-21 10:53:38 +0800

    enable mouse key

--- keyboards/nomouse/keymaps/default/keymap.c
+++ keyboards/nomouse/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,				KC_Q,			KC_W,			KC_E,			KC_R,			KC_T,							KC_Y,			KC_U,			KC_I,			KC_O,			KC_P,			KC_BSLS,	KC_HOME,
         KC_LCTL,			KC_A,			KC_S,			KC_D,			KC_F,			KC_G,							KC_H,			KC_J,			KC_K,			KC_L,			KC_SCLN,		KC_QUOT,	KC_END,	
         KC_LSFT,			KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,							KC_N,			KC_M,			KC_COMM,		KC_DOT,			KC_SLSH,		KC_RSFT,			
-        KC_CAPS,			MO(1),		    KC_LALT,		LT(2,KC_SPC),	KC_PSLS,														M_MOUSE_MBTN,	KC_ENT,			MO(3),		    KC_RGUI,		KC_RCTL
+        KC_CAPS,			MO(1),		    KC_LALT,		LT(2,KC_SPC),	MS_BTN1,														M_MOUSE_MBTN,	KC_ENT,			MO(3),		    KC_RGUI,		KC_RCTL
 
     ),
 
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,            C(G(KC_6)),	    C(G(KC_7)),	    C(G(KC_8)),	    C(G(KC_9)),	    C(G(KC_0)),						KC_TRNS,		KC_TRNS,		KC_BSLS,		KC_TRNS,		KC_TRNS,		KC_TRNS,	KC_TRNS,
         KC_TRNS,            C(G(KC_1)),		C(G(KC_2)),		C(G(KC_3)),		C(G(KC_4)),		C(G(KC_5)),						KC_TRNS,		KC_TRNS,		KC_MINS,		KC_EQL,		    KC_TRNS,		KC_TRNS,	KC_TRNS,	
         KC_TRNS,            S(G(KC_1)),		S(G(KC_2)),		S(G(KC_3)),		S(G(KC_4)),		S(G(KC_5)),						KC_TRNS,		KC_TRNS,		KC_LBRC,		KC_RBRC,		KC_TRNS,		KC_TRNS,			
-        KC_TRNS,			KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_PMNS,														KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS
+        KC_TRNS,			KC_TRNS,		KC_TRNS,		KC_TRNS,		MS_BTN2,														KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS,		KC_TRNS
 
     ),
     [3] = LAYOUT( 
@@ -110,9 +110,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
         case M_MOUSE_MBTN:
         if (record->event.pressed) {
-            register_code(KC_PAST);
+            register_code(MS_BTN3);
         } else {
-            unregister_code(KC_PAST);
+            unregister_code(MS_BTN3);
         }
         break;
 
